f2e0340c8a698c39403376a94faf170ca2738c4f
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-01-09 03:20:35 +0500

    Add functions.zsh

---
+++ .zsh/functions.zsh
@@ -0,0 +1,51 @@
+# ---------------------------------------------------------------------------- #
+# Navigation
+# ---------------------------------------------------------------------------- #
+
+
+# makes a directory and changes to it
+function mkcd {
+	[[ -n "$1" ]] && mkdir -p "$1" && cd "$1"
+}
+
+# cds into specified repository located under $REPOSITORIES
+function repo {
+	cd "$REPOSITORIES/$1"
+}
+compctl -/ -W "$REPOSITORIES" repo
+
+# cds into the forefront Finder window
+function cdf {
+	cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
+}
+
+
+# ---------------------------------------------------------------------------- #
+# Development
+# ---------------------------------------------------------------------------- #
+
+
+# cleans Xcode's derived data
+function cleandd {
+	rm -rf ~/Library/Developer/Xcode/DerivedData
+}
+
+# activates Python virtual environment
+function actvenv {
+	if [[ -n "$1" ]]; then
+		source "$1/bin/activate"
+	else
+		source ./venv/bin/activate
+	fi
+}
+
+
+# ---------------------------------------------------------------------------- #
+# Utilties
+# ---------------------------------------------------------------------------- #
+
+
+# sources .zshrc file
+function reload {
+	source "$ZDOTDIR/.zshrc"
+}
