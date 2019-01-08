4272f1f389576bf41e25aa945400093cd1337893
Kirill Gashkov <k.e.gashkov@gmail.com>
2019-01-08 16:47:55 +0500

    Add aliases.zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,40 @@
+
+# enable aliases to be sudo'ed
+alias sudo="sudo "
+
+
+# ---------------------------------------------------------------------------- #
+# Abbreviations: built-in
+# ---------------------------------------------------------------------------- #
+
+
+# ls
+alias l='ls -1A'
+alias ll='ls -lh'
+alias la='ls -lhA'
+
+# clear
+alias cl='clear'
+
+
+# ---------------------------------------------------------------------------- #
+# Abbreviations: external
+# ---------------------------------------------------------------------------- #
+
+
+# git - version contol system
+alias ga='git add'
+alias gr='git reset HEAD'
+alias gc='git commit'
+alias gca='git commit --amend'
+alias gch='git checkout'
+alias gf='git fetch'
+alias gu='git pull'
+alias gp='git push'
+alias gb='git branch'
+alias gl='git log --graph'
+alias gs='git status -sb'
+alias gd='git diff'
+
+# micro - modern terminal-based editor
+alias mi='micro'
