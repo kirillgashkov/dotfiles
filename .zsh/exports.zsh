426fdbab6117248c5b4fee51ac046eccdd1da2e7
Kirill Gashkov <gashkov.k.e@gmail.com>
2019-01-09 22:44:00 +0500

    Add exports.zsh

---
+++ .zsh/exports.zsh
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------------- #
+# Directories
+# ---------------------------------------------------------------------------- #
+
+
+# `repo` will search this directory for repositories
+export REPOSITORIES="$HOME/Repositories"
+
+# where the dotfiles located
+export DOTFILES="$HOME/.dotfiles"
+
+
+# ---------------------------------------------------------------------------- #
+# Session defaults
+# ---------------------------------------------------------------------------- #
+
+
+# terminal
+export TERM='xterm-256color'
+
+# programs
+export EDITOR='micro'
+export PAGER='less'
+export VISUAL='micro'
+export BROWSER='open'
+
+# locale
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US:en_GB:en'
+export LC_TIME='en_GB.UTF-8'
