fc2ee3f249daaab18f4ef324d5b0615e0d91ce70
Kirill Gashkov <k.e.gashkov@gmail.com>
2019-06-14 18:47:06 +0500

    Remove strong reference between dotfiles repo and GUI applications

--- guifiles/iterm2/setup.zsh
+++ guifiles/iterm2/setup.zsh
@@ -2,7 +2,21 @@
 
 killall iTerm2
 
-# specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$0:A:h"
-# tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+cur_dir="$0:A:h"
+
+settings_filename="com.googlecode.iterm2.plist"
+settings_file="$HOME/Library/Preferences/$settings_filename"
+
+
+if [[ -f "$settings_file" ]]; then
+    echo "File $settings_file exists. Backing up..."
+    cp "$settings_file" "$cur_dir/$settings_filename.bak"
+fi
+
+cp "$cur_dir/$filename" "$HOME/Library/Preferences/$filename"
+
+
+unset cur_dir
+unset settings_filename
+unset settings_file
