ab63c41aa2d24ffe1bef0351e31f7b47466eb690
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-04-05 01:07:11 +0500

    Add convenience cd functions: dev, edu, work

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -8,6 +8,24 @@ function mcd {
 	[[ -n "$1" ]] && mkdir -p "$1" && cd "$1"
 }
 
+# cds into specified repository located under $LOCAL_DEV
+function dev {
+	cd "$LOCAL_DEV/$1"
+}
+compctl -/ -W "$LOCAL_DEV" dev
+
+# cds into specified repository located under $LOCAL_EDU
+function edu {
+	cd "$LOCAL_EDU/$1"
+}
+compctl -/ -W "$LOCAL_EDU" edu
+
+# cds into specified repository located under $LOCAL_WORK
+function work {
+	cd "$LOCAL_WORK/$1"
+}
+compctl -/ -W "$LOCAL_WORK" work
+
 # cds into the forefront Finder window
 function cdf {
 	cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
