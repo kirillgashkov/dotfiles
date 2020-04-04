1a0d52982b97960f8a665bc68573f97c3033c8be
Kirill Gashkov <gashkov.k.e@gmail.com>
2020-04-05 01:04:37 +0500

    Remove unused functions

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -8,12 +8,6 @@ function mcd {
 	[[ -n "$1" ]] && mkdir -p "$1" && cd "$1"
 }
 
-# cds into specified repository located under $REPOSITORIES
-function repo {
-	cd "$REPOSITORIES/$1"
-}
-compctl -/ -W "$REPOSITORIES" repo
-
 # cds into the forefront Finder window
 function cdf {
 	cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
@@ -47,24 +41,6 @@ function actvenv {
 		source ./venv/bin/activate
 	fi
 }
-
-# copies pre-made gitignore (found under $GITIGNORES) to a current directory
-function gitignore {
-	cp "$GITIGNORES/$1" ./.gitignore
-}
-compctl -f -W "$GITIGNORES" gitignore
-
-# copies pre-made readme (found under $READMES) to a current directory
-function readme {
-	cp "$READMES/$1" ./README.md
-}
-compctl -f -W "$READMES" readme
-
-# copies pre-made license (found under $LICENSES) to a current directory
-function license {
-	cp "$LICENSES/$1" ./LICENSE.md
-}
-compctl -f -W "$LICENSES" license
 
 
 # ---------------------------------------------------------------------------- #
