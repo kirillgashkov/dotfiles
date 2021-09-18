9aa050e6f8c5dbcf7719300ed0c13732d10cdd9f
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-09-18 19:12:30 +0300

    Add Mojave-specific setting to the MacBook setup script

--- scripts/computers/macbook.sh
+++ scripts/computers/macbook.sh
@@ -6,3 +6,6 @@ sudo -v
 # Set computer name
 scutil --set ComputerName "Cyril's MacBook"
 scutil --set LocalHostName 'cyrils-macbook'
+
+# macOS Mojave: stop iTunes from opening when an iPhone is connected
+defaults read com.apple.iTunes dontAutomaticallySyncIPods -bool true
