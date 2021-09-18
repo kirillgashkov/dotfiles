2c3208adf054f22cea76dab4e52019034738be0a
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-09-18 17:49:36 +0300

    Make macos.sh script run through /bin/sh
    It should be able to run on a POSIX shell.

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we're about to change
@@ -8,7 +8,7 @@ osascript -e 'tell application "System Preferences" to quit'
 sudo -v
 
 # Keep-alive: update existing 'sudo' time stamp until this script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2> /dev/null &
 
 ###############################################################################
 # General UI/UX                                                               #
