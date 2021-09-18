d516fb009226674a79a65d430216e4aa3c48956c
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-09-18 11:56:18 +0300

    Choose 'Safari' settings for macOS again

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -125,16 +125,8 @@ defaults write com.apple.dock show-recents -bool false
 # Safari & WebKit                                                             #
 ###############################################################################
 
-# Enable Safari's debug menu
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-# Enable the Develop menu and the Web Inspector in Safari
+# Enable the Develop menu in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-
-# Add a context menu item for showing the Web Inspector in web views
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 ###############################################################################
 # Time Machine                                                                #
