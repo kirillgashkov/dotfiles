27ea85b5079d361b0237986551bea8f1cbfc1b8d
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-09-23 12:30:09 +0300

    Clean up aliases
    Prefer double quotes.

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -3,26 +3,26 @@ alias sudo="sudo "
 
 # Default options
 
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
 
 # Abbreviations
 
-alias l='ls -1A'
-alias ll='ls -lFA'
+alias l="ls -1A"
+alias ll="ls -lFA"
 
-alias c='clear'
+alias c="clear"
 
-alias ga='git add'
-alias gb='git branch'
-alias gc='git commit'
-alias gca='git commit --amend'
-alias gco='git checkout'
-alias gd='git diff'
-alias gds='git diff --staged'
-alias gf='git fetch'
-alias gl='git --no-pager log --graph --oneline -n 20'
-alias gp='git push'
-alias gr='git reset HEAD'
-alias gs='git status -sb'
-alias gu='git pull'
+alias ga="git add"
+alias gb="git branch"
+alias gc="git commit"
+alias gca="git commit --amend"
+alias gco="git checkout"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gf="git fetch"
+alias gl="git --no-pager log --graph --oneline -n 20"
+alias gp="git push"
+alias gr="git reset HEAD"
+alias gs="git status -sb"
+alias gu="git pull"
