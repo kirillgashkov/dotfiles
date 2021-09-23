7bc196e8b35ada27ba26da9d4d13f00e2840742a
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-09-23 23:20:46 +0300

    Simplify 'pyenv' init

--- dotfiles/zsh/inits/pyenv.zsh
+++ dotfiles/zsh/inits/pyenv.zsh
@@ -1,29 +1,19 @@
-# Lazy init pyenv either through the command or through the completion
-_pyenv_init() {
-    # The second run of this function will be harmful to the environment
-    unfunction _pyenv_init
-
-    # Remove lazy functions and completers in favor of whatever the init brings
-    unfunction pyenv
-    unfunction _pyenv
+_pyenv_uninstall_lazy_init() {
+    unset -f pyenv
+    unset -f _pyenv
     compdef -d pyenv
-
-    # Initialize pyenv to get new 'pyenv' function and completions
-    eval "$(pyenv init -)"
 }
+
 pyenv() {
-    # Intercept pyenv call to perform initialization
-    _pyenv_init
-
-    # Perform the real pyenv call after initialization
+    _pyenv_uninstall_lazy_init
+    eval "$(pyenv init -)"
     pyenv "$@"
-
 }
-_pyenv() {
-    # Intercept pyenv completion to perform initialization
-    _pyenv_init
 
-    # Perform the real pyenv completion after initialization
+_pyenv() {
+    _pyenv_uninstall_lazy_init
+    eval "$(pyenv init -)"
     _main_complete
 }
+
 compdef _pyenv pyenv
