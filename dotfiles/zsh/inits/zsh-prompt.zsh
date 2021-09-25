11a90bb6f9ea378d1fedd55683d5598aa520671b
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-09-25 22:51:55 +0300

    Make 'zsh-prompt' init more readable

--- dotfiles/zsh/inits/zsh-prompt.zsh
+++ dotfiles/zsh/inits/zsh-prompt.zsh
@@ -1,44 +1,32 @@
 _prompt_precmd() {
-    local exit_status="$?"
+    local exit_status="$?" git_ref git_commit
+    local username hostname branch venv workdir symbol
+    
+    workdir="%B%F{cyan}%~%f%b"
+    symbol="%B%F{green}❯%f%b"
 
-    local username=""
-    local hostname=""
-    local workdir="%B%F{cyan}%~%f%b"
-    local branch=""
-    local venv=""
-    local symbol="%B%F{green}❯%f%b"
+    git_ref="$(git symbolic-ref --quiet HEAD 2> /dev/null)"
+    [[ "$?" -eq 1 ]] && git_commit="$(git rev-parse --short HEAD 2> /dev/null)"
 
-    # Show username and hostname when connected via SSH
+    if [[ -n "$git_ref" ]]; then
+        branch=" on %B%F{magenta}${git_ref#refs/heads/}%f%b"
+    elif [[ -n "$git_commit" ]]; then
+        branch=" on %B%F{magenta}HEAD%f%b %B%F{green}($git_commit)%b%f"
+    fi
+
     if [[ -n "${SSH_CONNECTION-}${SSH_CLIENT-}${SSH_TTY-}" ]]; then
         username="%B%F{yellow}%n%f%b in "
         hostname="%B%F{green}%m%f%b in "
     fi
 
-    # Show red username for superuser
     if [[ "${EUID-}" -eq 0 ]]; then
         username="%B%F{red}%n%f%b in "
     fi
 
-    # Show branch when in a Git repository
-    local ref ref_status
-    ref="$(command git symbolic-ref --quiet HEAD 2> /dev/null)"
-    ref_status="$?"
-    if [[ "$ref_status" -eq 128 ]]; then
-        # No Git repository here
-    elif [[ "$ref_status" -eq 0 ]]; then
-        # Ref variable contains the current branch
-        branch=" on %B%F{magenta}${ref#refs/heads/}%f%b"
-    elif ref="$(command git rev-parse --short HEAD 2> /dev/null)"; then
-        # HEAD is in a detached state
-        branch=" on %B%F{magenta}HEAD%f%b %B%F{green}($ref)%b%f"
-    fi
-
-    # Show venv when it's activated
     if [[ -n "${VIRTUAL_ENV-}" ]]; then
         venv=" via %B%F{yellow}${VIRTUAL_ENV:t}%f%b"
     fi
 
-    # Color symbol red if previous command failed
     if [[ "$exit_status" -ne 0 ]]; then
         symbol="%B%F{red}❯%f%b"
     fi
