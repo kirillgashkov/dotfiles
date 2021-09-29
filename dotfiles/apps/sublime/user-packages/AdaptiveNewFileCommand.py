4be56ab16d65c153f52cd87efcb83ed7d2409b62
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-09-29 19:17:27 +0300

    Add 'Sublime Text' app preferences

---
+++ dotfiles/apps/sublime/user-packages/AdaptiveNewFileCommand.py
@@ -0,0 +1,12 @@
+from sublime_plugin import WindowCommand
+
+
+class AdaptiveNewFileCommand(WindowCommand):
+    """
+    Create a new file inheriting syntax from the current one.
+    """
+    def run(self):
+        current_window = self.window
+        current_syntax = current_window.active_view().settings().get("syntax")
+        new_view = current_window.new_file()
+        new_view.set_syntax_file(current_syntax)
