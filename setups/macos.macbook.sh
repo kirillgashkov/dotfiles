963867bd367fa4abf33237f7402c9be52f127806
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-09-30 21:40:01 +0300

    Make macos.macbook.sh script standalone
    Previously it was a little dependent on the macos.sh script (or
    probably didn't even work well on its own), now it is independent.

--- setups/macos.macbook.sh
+++ setups/macos.macbook.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we're about to change
+osascript -e 'tell application "System Preferences" to quit'
+
 # Ask for the administrator password upfront
 sudo -v
+
+# Keep-alive: update existing 'sudo' time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2> /dev/null &
+
+
+#
+# Preferences
+#
+
 
 # Set computer name
 scutil --set ComputerName "Cyril's MacBook"
@@ -9,3 +22,14 @@ scutil --set LocalHostName 'cyrils-macbook'
 
 # macOS Mojave: stop iTunes from opening when an iPhone is connected
 defaults read com.apple.iTunes dontAutomaticallySyncIPods -bool true
+
+
+#
+# Cleanup
+#
+
+
+# Kill affected applications
+for app in 'iTunes'; do
+    killall "$app" &> /dev/null
+done
