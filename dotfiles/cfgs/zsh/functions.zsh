7c0d729bfe8ba61ec215122ec3161b081bb9d1a2
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-10-01 10:21:50 +0300

    Use GNU ls by default via a wrapper function

--- dotfiles/cfgs/zsh/functions.zsh
+++ dotfiles/cfgs/zsh/functions.zsh
@@ -1,3 +1,8 @@
+# Use GNU ls by default
+ls() {
+    /usr/local/bin/gls "$@"
+}
+
 # Change working directory to the top-most Finder window location
 cdf() {
     cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
