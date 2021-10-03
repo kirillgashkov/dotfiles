6bdf60d15c7c9b2c91c5afa848aeb12426889c8a
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-10-03 10:44:42 +0300

    Add argument parsing and usage the main setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,31 @@
 #!/bin/sh
+
+usage() {
+    cat >&2 << EOF
+Usage: $0 [options]
+
+Options:
+  -h, --help      Show this message.
+  --[no-]macbook  Run/skip MacBook specific setups.
+EOF
+}
+
+macbook=0
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        --macbook)
+            macbook=1
+            shift
+            ;;
+        *)
+            echo >&2 "Unknown option: $1"
+            usage
+            exit 0
+            ;;
+    esac
+done
