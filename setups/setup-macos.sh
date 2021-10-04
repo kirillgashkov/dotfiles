c1db49f255aad02871f6d5691c1f90df0f570be0
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-10-04 15:45:46 +0300

    Reorder some lines in macOS setups

--- setups/setup-macos.sh
+++ setups/setup-macos.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we're about to change
-osascript -e 'tell application "System Preferences" to quit'
-
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing 'sudo' time stamp until this script has finished
+# Keep-alive: update existing 'sudo' timestamp until this script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2> /dev/null &
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we're about to change
+osascript -e 'tell application "System Preferences" to quit'
 
 
 #
