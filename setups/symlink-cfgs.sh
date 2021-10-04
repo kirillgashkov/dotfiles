67a754c12b94c01677684b3a3fa3402b34ffb4e5
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-10-04 14:28:38 +0300

    Add new things from symlink-bins to symlink-cfgs
    - Name '$1' sooner
    - Require users to provide abspaths
    - Fail if couldn't provide ~/.config

--- setups/symlink-cfgs.sh
+++ setups/symlink-cfgs.sh
@@ -5,15 +5,25 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-if [ ! -d "$1" ]; then
+dotfiles_cfgs="$1"
+
+if [ ! -d "$dotfiles_cfgs" ]; then
     echo >&2 "$(basename "$0"): provided 'cfgs' directory is not a directory"
     exit 1
 fi
 
-dotfiles_cfgs="$1"
+if [ "$(printf '%s' "$dotfiles_cfgs" | cut -c1)" != "/" ]; then
+    echo >&2 "$(basename "$0"): 'cfgs' directory must be provided via an absolute path"
+    exit 1
+fi
+
 local_cfgs="$HOME/.config"
 
 mkdir -p "$local_cfgs"
+if [ ! -d "$local_cfgs" ]; then
+    echo >&2 "$(basename "$0"): failed to provide local config directory for symlinks"
+    exit 1
+fi
 
 for cfg in "$dotfiles_cfgs/"*; do
     filename="$(basename "$cfg")"
