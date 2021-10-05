1163ec8961853c9bd72a3cb21a2bfc5c1c2b8775
Kirill Gashkov <k.e.gashkov@gmail.com>
2021-10-05 12:28:16 +0300

    Update section comments in setup-apps setup

--- setups/setup-apps.sh
+++ setups/setup-apps.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-#
-# Transmission
-#
-
+# Set Transmission preferences
 
 defaults write org.m0k.transmission DownloadAsk -bool false                 # Don't ask before starting a download
 defaults write org.m0k.transmission MagnetOpenAsk -bool false               # Don't ask before opening a magnet link
@@ -14,41 +11,24 @@ defaults write org.m0k.transmission WarningDonate -bool false               # Hi
 defaults write org.m0k.transmission WarningLegal -bool false                # Hide the legal disclaimer
 defaults write org.m0k.transmission RandomPort -bool true                   # Randomize port on launch
 
-
-#
-# Amphetamine
-#
-
+# Set Amphetamine preferences
 
 defaults write com.if.Amphetamine "Show Welcome Window" -bool false         # Don't show welcome window
 defaults write com.if.Amphetamine "Enable Session State Sound" -bool false  # Don't play sound when any session starts or ends
 defaults write com.if.Amphetamine "Icon Style" -int 5                       # Set menu bar icon to a coffee cup
 
-
-#
-# Rectangle
-#
-
-
+# Set Rectangle preferences
 
 defaults write com.knollsoft.Rectangle alternateDefaultShortcuts -int 1     # Set shortcuts to Rectangle defaults
 defaults write com.knollsoft.Rectangle launchOnLogin -bool false            # Don't launch Rectangle on login
 defaults write com.knollsoft.Rectangle subsequentExecutionMode -int 1       # Move to adjacent display on repeated left or right commands
 defaults write com.knollsoft.Rectangle windowSnapping -int 2                # Turn off window snapping
 
-
-#
-# Flow
-#
-
+# Set Flow preferences
 
 defaults write design.yugen.Flow showWelcomeWindow -bool false              # Don't show welcome window
 
-
-#
-# Telegram
-#
-
+# Set Telegram preferences
 
 defaults write ru.keepcoder.Telegram kArchiveIsHidden -bool true            # Hide archived chats from All Chats
 
