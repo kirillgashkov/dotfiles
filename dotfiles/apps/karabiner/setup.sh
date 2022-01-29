cca36d153d01e70872b828480329987dea9cb122
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-01-29 13:16:17 +0300

    Symlink the whole config directory for karabiner
    Because it would seem that karabiner likes to replace my
    karabiner.json symlink with its own brand new file.

--- dotfiles/apps/karabiner/setup.sh
+++ dotfiles/apps/karabiner/setup.sh
@@ -2,17 +2,17 @@
 
 dotfiles_app_dir="$(cd -- "$(dirname -- "$0")" > /dev/null && pwd)"
 
-local_app_config_dir="$HOME/.config/karabiner"
+local_config_dir="$HOME/.config"
 
-mkdir -p "$local_app_config_dir"
-if [ ! -d "$local_app_config_dir" ]; then
-    echo >&2 "$(basename "$0"): failed to provide local app config directory for symlinks"
+mkdir -p "$local_config_dir"
+if [ ! -d "$local_config_dir" ]; then
+    echo >&2 "$(basename "$0"): failed to provide config directory for symlinks"
     exit 1
 fi
 
-for filename in "assets" "karabiner.json"; do
+for filename in "karabiner"; do
     echo "$(tput setaf 3)Symlinking $filename$(tput sgr0)"
-    ln -s "$dotfiles_app_dir/$filename" "$local_app_config_dir"
+    ln -s "$dotfiles_app_dir/$filename" "$local_config_dir"
 
     if [ "$?" -ne 0 ]; then
         echo "$(tput setaf 1)Symlinking $filename has failed$(tput sgr0)"
