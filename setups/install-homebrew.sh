df047f8fe15ab14bb15af37c9034e96fc11bdad9
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-01-29 11:36:16 +0300

    Add a check for an already installed Homebrew

--- setups/install-homebrew.sh
+++ setups/install-homebrew.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if command -v brew &> /dev/null; then
+    echo "Already installed."
+    exit 0
+fi
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
