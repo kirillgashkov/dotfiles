8fec65726c36ddb9533d13d3f008868924f84bf2
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-03-19 15:35:12 +0300

    Turn off quick note feature on macOS

--- setups/setup-macos.sh
+++ setups/setup-macos.sh
@@ -23,6 +23,7 @@ defaults write com.apple.dock show-recents -bool false                          
 defaults write com.apple.dock persistent-apps -array                               # Remove all (default) icons from Dock
 defaults write com.apple.dock autohide-delay -float 0.2                            # Speed up the Dock's auto-hiding
 defaults write com.apple.dock autohide-time-modifier -float 0.7                    # Speed up the Dock's hiding/showing animation
+defaults write com.apple.dock wvous-br-corner -int 1                               # Turn off quick note feature
 
 # Set language and region preferences
 
