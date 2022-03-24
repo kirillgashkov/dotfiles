8ef555e31f2edc6cc9bf23156a335ca17f1908ca
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-03-25 00:02:08 +0300

    Change cursor shape depending on the Vim mode

--- dotfiles/cfgs/zsh/inits/zsh-bindings.zsh
+++ dotfiles/cfgs/zsh/inits/zsh-bindings.zsh
@@ -1,1 +1,21 @@
-bindkey -v  # Vim key bindings
+# Use Vim key bindings
+
+bindkey -v
+
+# Change cursor depending on the current Vim mode
+
+zle-keymap-select() {
+    if [[ "$KEYMAP" == "vicmd" || "$1" == "block" ]]; then
+        echo -ne "\e[2 q"
+    elif [[ -z "$KEYMAP" || "$KEYMAP" == "main" || "$KEYMAP" == "viins" || "$1" == "beam" ]]; then
+        echo -ne "\e[6 q"
+    fi
+}
+zle -N zle-keymap-select
+
+_cursor_precmd() {
+   echo -ne "\e[6 q"
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd _cursor_precmd
