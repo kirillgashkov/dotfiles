a40c3b2853e3ed249ac20f7e4b397e3095a9b4bf
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-04-12 02:34:00 +0300

    Set keyboard shortcuts

--- setups/setup-macos.sh
+++ setups/setup-macos.sh
@@ -47,6 +47,31 @@ defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false  
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false      # Disable smart quotes
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false       # Disable smart dashes
 
+# Set keyboard shortcuts
+
+/usr/libexec/PlistBuddy \
+    -c "Delete :AppleSymbolicHotKeys:60" \
+    -c "Add :AppleSymbolicHotKeys:60:enabled bool true" \
+    -c "Add :AppleSymbolicHotKeys:60:value dict" \
+    -c "Add :AppleSymbolicHotKeys:60:value:parameters array" \
+    -c "Add :AppleSymbolicHotKeys:60:value:parameters: integer 65535" \
+    -c "Add :AppleSymbolicHotKeys:60:value:parameters: integer 80" \
+    -c "Add :AppleSymbolicHotKeys:60:value:parameters: integer 8388608" \
+    -c "Add :AppleSymbolicHotKeys:60:value:type string standard" \
+    "$HOME/Library/Preferences/com.apple.symbolichotkeys.plist"                    # Map "Select the previous input source" shortcut to F19 (which is managed by Karabiner)
+
+/usr/libexec/PlistBuddy \
+    -c "Set :AppleSymbolicHotKeys:61:enabled bool false" \
+    "$HOME/Library/Preferences/com.apple.symbolichotkeys.plist"                    # Disable "Select the next source in Input menu" shortcut
+
+/usr/libexec/PlistBuddy \
+    -c "Set :AppleSymbolicHotKeys:64:enabled bool false" \
+    "$HOME/Library/Preferences/com.apple.symbolichotkeys.plist"                    # Disable "Show Spotlight search" shortcut (in favor of Alfred)
+
+/usr/libexec/PlistBuddy \
+    -c "Set :AppleSymbolicHotKeys:65:enabled bool false" \
+    "$HOME/Library/Preferences/com.apple.symbolichotkeys.plist"                    # Disable "Show Finder search window" shortcut
+
 # Set trackpad preferences
 
 defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 0        # Make trackpad click feel light (also set SecondClickThreshold)
