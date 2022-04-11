670f2a5ad841dc041d14c68aa4fbc6d72556a8f9
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-04-12 02:32:43 +0300

    Fix potential bug in setup-macos.sh
    A comment after '\' could mess with the command.

--- setups/setup-macos.sh
+++ setups/setup-macos.sh
@@ -26,11 +26,11 @@ defaults write com.apple.dock wvous-br-corner -int 1                            
 defaults write NSGlobalDomain AppleLocale -string "en_US"                          # Region: US
 defaults write NSGlobalDomain AppleLanguages -array "en-US" "ru-RU"                # Preferred languages: English (US), Russian
 defaults write NSGlobalDomain AppleICUForce12HourTime -bool true                   # 12-hour time
-defaults write NSGlobalDomain AppleICUNumberSymbols -dict \                        # US number separators
+defaults write NSGlobalDomain AppleICUNumberSymbols -dict \
     0 -string "." \
     1 -string "," \
     10 -string "." \
-    17 -string ","
+    17 -string ","                                                                 # US number separators
 defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"          # Metric measurement units
 defaults write NSGlobalDomain AppleMetricUnits -bool true                          # Metric measurement units
 defaults write NSGlobalDomain AppleTemperatureUnit -string "Celsius"               # Temperature: Celsius
