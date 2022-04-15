53ef779d24b028a1d10b9590c13d32f33620f5d5
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-04-16 01:01:00 +0300

    Update rectangle defaults

--- setups/setup-rectangle.sh
+++ setups/setup-rectangle.sh
@@ -18,7 +18,8 @@ require_macos_application "Rectangle"
 
 defaults write com.knollsoft.Rectangle alternateDefaultShortcuts -int 1  # Set shortcuts to Rectangle defaults
 defaults write com.knollsoft.Rectangle launchOnLogin -bool false         # Don't launch Rectangle on login
-defaults write com.knollsoft.Rectangle subsequentExecutionMode -int 1    # Move to adjacent display on repeated left or right commands
+defaults write com.knollsoft.Rectangle hideMenubarIcon -bool true        # Hide menu bar icon
+defaults write com.knollsoft.Rectangle subsequentExecutionMode -int 2    # Do nothing
 defaults write com.knollsoft.Rectangle windowSnapping -int 2             # Turn off window snapping
 
 killall "Rectangle" &> /dev/null
