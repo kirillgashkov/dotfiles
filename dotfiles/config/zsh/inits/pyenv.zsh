20e65f15998c3ce6b5955f8e96bcf54df3829eac
Kirill Gashkov <k.e.gashkov@gmail.com>
2023-04-23 22:24:24 +0300

    Refactor pyenv's init

--- dotfiles/config/zsh/inits/pyenv.zsh
+++ dotfiles/config/zsh/inits/pyenv.zsh
@@ -1,18 +1,19 @@
-_pyenv_uninstall_lazy_init() {
+_init_pyenv_once() {
+    unset -f _init_pyenv_once
     unset -f pyenv
     unset -f _pyenv
     compdef -d pyenv
+
+    eval "$(pyenv init -)"
 }
 
 pyenv() {
-    _pyenv_uninstall_lazy_init
-    eval "$(pyenv init -)"
+    _init_pyenv_once
     pyenv "$@"
 }
 
 _pyenv() {
-    _pyenv_uninstall_lazy_init
-    eval "$(pyenv init -)"
+    _init_pyenv_once
     _main_complete
 }
 
