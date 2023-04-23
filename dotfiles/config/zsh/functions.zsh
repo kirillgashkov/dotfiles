e9f5a9d576e7c1949a1861a6f8080f43be856996
Kirill Gashkov <k.e.gashkov@gmail.com>
2023-04-23 23:03:52 +0300

    Use GNU strings by default

--- dotfiles/config/zsh/functions.zsh
+++ dotfiles/config/zsh/functions.zsh
@@ -13,6 +13,11 @@ git() {
     /usr/local/bin/git "$@"
 }
 
+# Use GNU strings by default
+strings() {
+    /usr/local/opt/binutils/bin/strings "$@"
+}
+
 # Change working directory to the top-most Finder window location
 cdf() {
     cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
