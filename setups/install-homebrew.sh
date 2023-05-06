bc594530938b55900dc99f9df261994e1a316f03
Kirill Gashkov <k.e.gashkov@gmail.com>
2023-05-06 21:43:50 +0300

    Sort imports in setups

--- setups/install-homebrew.sh
+++ setups/install-homebrew.sh
@@ -4,8 +4,8 @@ if [ -z "$DOTFILES_SETUP_MIXINS" ]; then
     echo >&2 "$(basename "$0"): DOTFILES_SETUP_MIXINS wasn't passed to the setup"
     exit 1
 fi
-source "$DOTFILES_SETUP_MIXINS/require.sh"
 source "$DOTFILES_SETUP_MIXINS/exit.sh"
+source "$DOTFILES_SETUP_MIXINS/require.sh"
 
 
 require_environment
