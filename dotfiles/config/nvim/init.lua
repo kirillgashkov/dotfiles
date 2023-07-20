dea94c0edd6d421056c1991d1b21be55b0627553
Kirill Gashkov <k.e.gashkov@gmail.com>
2023-07-21 01:11:34 +0500

    Format

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -10,16 +10,16 @@ vim.g.maplocalleader = " "
 
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazy_path) then
-  vim.print("Installing lazy")
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazy_path,
-  })
-  vim.print("Installed lazy")
+	vim.print("Installing lazy")
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazy_path,
+	})
+	vim.print("Installed lazy")
 end
 vim.opt.rtp:prepend(lazy_path)
 
