44792ce44e77b678ad7bef7fac1df5619d3c1416
Kirill Gashkov <k.e.gashkov@gmail.com>
2023-07-25 15:40:20 +0500

    Update snippets

--- dotfiles/config/nvim/lua/snippets/all.lua
+++ dotfiles/config/nvim/lua/snippets/all.lua
@@ -1,23 +1,8 @@
 local luasnip = require("luasnip")
 local snippet = luasnip.snippet
 local text_node = luasnip.text_node
-local insert_node = luasnip.insert_node
 
 return {
-	snippet("shrugs", { text_node([[¯\_(ツ)_/¯]] .. "\n") }),
-	snippet("cowsay", {
-		text_node([[            ^__^  /]] .. " "),
-		insert_node(1, "Moo?"),
-		text_node(
-			"\n"
-				.. [[    _______/(oo) /]]
-				.. "\n"
-				.. [[/\/)       /(__)]]
-				.. "\n"
-				.. [[   | w----||]]
-				.. "\n"
-				.. [[   ||     ||]]
-				.. "\n"
-		),
-	}),
+	snippet("shrugs", { text_node([[¯\_(ツ)_/¯]]) }),
+	snippet("shrugs2", { text_node([[¯\_( ͡° ͜ʖ ͡°)_/¯]]) }),
 }
