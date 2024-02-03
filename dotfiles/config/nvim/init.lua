656fa7c3fda240fa07d32531fe15f452bd613caa
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-03 15:48:20 +0300

    Configure Neovim according to Vim Galore

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,3 +1,17 @@
--- vim.api.nvim_set_option("clipboard", "unnamed")
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.expandtab = true -- Use the appropriate number of spaces to insert a <Tab>
+vim.opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>
+vim.opt.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+
+vim.opt.number = true -- Print the line number in front of each line
+vim.opt.relativenumber = true -- Show the line number relative to the line with the cursor in front of each line
+
+vim.opt.list = true -- Show non-printable characters. Recommended by Vim Galore
+vim.opt.listchars = { tab = "→ ", space = "·" } -- Strings to use for non-printable characters
+
+vim.opt.report = 0 -- Threshold for reporting number of lines changed. Recommended by Vim Galore
+
+-- Allow switching your keyboard into a special language mode
+vim.opt.langmap = {
+	"ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯфисвуапршолдьтщзйкыегмцчня;ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+}
