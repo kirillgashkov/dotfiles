4414c4c6b8adc4e5162c79ec6cdef90bd43ae652
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-06 16:01:44 +0300

    Configure syntax highlighting and color scheme in Neovim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -21,3 +21,67 @@ vim.opt.langmap = {
 }
 
 vim.opt.shellcmdflag = "-i " .. vim.opt.shellcmdflag:get() -- Use interactive shell for :!
+
+
+
+--
+-- Plugins
+--
+
+
+vim.loader.enable()
+
+local plugins_dir = vim.fn.stdpath("data") .. "/site/pack/dotfiles/opt"
+if not vim.loop.fs_stat(plugins_dir) then
+    print("Creating the plugins directory")
+    vim.fn.system({ "mkdir", "-p", plugins_dir })
+end
+
+
+-- Syntax Highlighting
+
+local should_update_nvim_treesitter_parsers = false
+
+if not vim.loop.fs_stat(plugins_dir .. "/nvim-treesitter") then
+    print("Installing nvim-treesitter/nvim-treesitter")
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/nvim-treesitter/nvim-treesitter.git",
+        plugins_dir .. "/nvim-treesitter",
+    })
+    should_update_nvim_treesitter_parsers = true
+end
+
+vim.cmd.packadd("nvim-treesitter")
+
+require("nvim-treesitter.configs").setup({
+    ensure_installed = {"markdown", "markdown_inline"},
+    sync_install = true,
+    auto_install = true,
+    highlight = { enable = true },
+})
+
+if should_update_nvim_treesitter_parsers then
+    print("Updating nvim-treesitter/nvim-treesitter parsers")
+    vim.cmd.TSUpdate()
+end
+
+
+-- Color Scheme
+
+if not vim.loop.fs_stat(plugins_dir .. "/tokyonight.nvim") then
+    print("Installing folke/tokyonight.nvim")
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/tokyonight.nvim.git",
+        plugins_dir .. "/tokyonight.nvim",
+    })
+end
+
+vim.cmd.packadd("tokyonight.nvim")
+
+vim.cmd.colorscheme("tokyonight-night")
