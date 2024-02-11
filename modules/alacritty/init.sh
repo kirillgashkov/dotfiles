bcc0f0208eea46358261810e96e175f8c8693f7a
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-11 15:27:00 +0300

    Add more 'alacritty' requirements

--- modules/alacritty/init.sh
+++ modules/alacritty/init.sh
@@ -4,7 +4,10 @@ set -e
 
 module_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
-# Requires 'tokyonight.nvim' (because of the color scheme import)
+# Config requires tokyonight.nvim (repository at ~/.local/share/nvim/lazy/tokyonight.nvim)
+# Config requires font-jetbrains-mono-nerd-font (font named 'JetBrainsMono Nerd Font Mono')
+# Config requries tmux (executable at /usr/local/bin/tmux)
+# Config requires tmux (key bindings)
 install() {
   brew install --cask alacritty
   test ! -e "$XDG_CONFIG_HOME/alacritty"
