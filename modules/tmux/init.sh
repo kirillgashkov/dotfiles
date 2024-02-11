01e924b422ea765e24b60b15c1579e93dff5d0d3
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-11 15:17:05 +0300

    Add more 'tmux' requirements

--- modules/tmux/init.sh
+++ modules/tmux/init.sh
@@ -8,6 +8,12 @@ module_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 # Requires 'XDG_CONFIG_HOME' variable
 # Requires 'XDG_CONFIG_HOME' directory existence
 # Requires alacritty (because of 'set-option -sa terminal-features ",alacritty:RGB"')
+# Requires 'tmux-256-color' (because of 'set-option -g default-terminal "tmux-256color"')
+# Requires fzf at /usr/local/bin/fzf (config)
+# Requires tmux at /usr/local/bin/tmux (config)
+# Requires xargs at /usr/bin/xargs (config)
+# Requires echo (config)
+# Requires (maybe) sh (config, because we use tmux commands that invoke shell)
 # Provides tmux
 install() {
   brew install tmux
