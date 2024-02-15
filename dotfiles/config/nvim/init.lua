a0bcdd549ac8baadf599644bee4b6b778e60da2d
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-15 03:50:01 +0300

    Remove tabline from Neovim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -92,14 +92,6 @@ require("lazy").setup({
 					lualine_y = {},
 					lualine_z = {},
 				},
-				tabline = {
-					lualine_a = { "buffers" },
-					lualine_b = {},
-					lualine_c = {},
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = { "tabs" },
-				},
 			})
 		end,
 	},
