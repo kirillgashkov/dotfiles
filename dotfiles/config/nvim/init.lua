2c751cfb3b8c87ea69ee6ff68b6dc899257cffa5
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-19 22:31:26 +0300

    Install and configure 'luasnip' in Neovim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -146,4 +146,59 @@ require("lazy").setup({
 			end, { silent = true })
 		end,
 	},
+  {
+    "https://github.com/L3MON4D3/LuaSnip",
+    version = "v2.*",
+    lazy = false,
+    build = function()
+      vim.fn.system({ "make", "install_jsregexp" })
+    end,
+    config = function()
+      require("init_luasnip_snippets")
+
+      local maybe_expand = function()
+        if not require("luasnip").expandable() then
+          return false
+        end
+        require("luasnip").expand()
+        return true
+      end
+
+      local maybe_jump = function(direction)
+        if not require("luasnip").jumpable(direction) then
+          return false
+        end
+        require("luasnip").jump(direction)
+        return true
+      end
+
+      local maybe_choose = function(direction)
+        if not require("luasnip").choice_active() then
+          return false
+        end
+        require("luasnip").change_choice(direction)
+        return true
+      end
+
+			vim.keymap.set({ "i", "s" }, "<Tab>", function()
+        if not maybe_expand() then
+          maybe_jump(1)
+        end
+			end, { silent = true })
+
+			vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+        maybe_jump(-1)
+			end, { silent = true })
+
+			vim.keymap.set({ "i", "s" }, "<M-Tab>", function()
+        if not maybe_expand() then
+          maybe_choose(1)
+        end
+			end, { silent = true })
+
+			vim.keymap.set({ "i", "s" }, "<S-M-Tab>", function()
+        maybe_choose(-1)
+			end, { silent = true })
+    end,
+  }
 })
