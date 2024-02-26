8d0ef4e457cd19c2528402206a08a190e796d1de
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-27 01:00:23 +0300

    Configure proxy for Copilot in Neovim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -246,10 +246,38 @@ require("lazy").setup({
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
-      require("copilot").setup({
+      vim.g.copilot_proxy = "http://127.0.0.1:1081"
+
+      -- Manually perform `require("copilot").setup()` to activate the panel
+      -- and suggestions modules only when the Copilot client is attached
+      -- (which means the Copilot server has been started and configured). This
+      -- is necessary for the plugin to respect the proxy setting from the very
+      -- beginning.
+      --
+      -- See:
+      --
+      -- - https://github.com/zbirenbaum/copilot.lua/blob/master/lua/copilot/init.lua
+      -- - https://github.com/zbirenbaum/copilot.lua/blob/master/lua/copilot/command.lua
+      require("copilot.config").setup({
         suggestion = { auto_trigger = true, keymap = { accept = "<M-y>", dismiss = "<M-e>" } },
         filetypes = { ["*"] = true },
       })
+      require("copilot.highlight").setup()
+
+      vim.api.nvim_create_autocmd({ "LspAttach" }, {
+        group = vim.api.nvim_create_augroup("user_copilot", {}),
+        callback = function(event)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.name == "copilot" then
+            require("copilot.panel").setup()
+            require("copilot.suggestion").setup()
+            require("copilot").setup_done = true
+            return true  -- Stop the autocmd from running again
+          end
+        end,
+      })
+
+      require("copilot.client").setup()
     end,
   },
 	{
