7d928dd8c0842bb4c5da0df54b5b99af70d642b8
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-05 23:28:02 +0300

    Factor out Brewfile

--- modules/brew/init.sh
+++ modules/brew/init.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -e
+
+module_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+
 # Requires bash
 # Requires curl
 # Requires interaction
@@ -7,6 +11,8 @@
 # Provides brew
 install() {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  brew bundle install --file "$module_dir/Brewfile"
 }
 
 install
