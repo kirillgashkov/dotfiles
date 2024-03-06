7bef9304a5b0c78e1db5d26a093b0c1577545af6
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-06 23:09:26 +0300

    Remove redundant mas installs

--- modules/amphetamine/init.sh
+++ modules/amphetamine/init.sh
@@ -5,7 +5,6 @@ set -e
 module_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
 install() {
-  mas install 937984704  # Amphetamine
   defaults write com.if.Amphetamine "Show Welcome Window" -bool false  # Don't show welcome window
   defaults write com.if.Amphetamine "Enable Session State Sound" -bool false  # Don't play sound when any session starts or ends
   defaults write com.if.Amphetamine "Icon Style" -int 5  # Set menu bar icon to a coffee cup
--- modules/flow/init.sh
+++ modules/flow/init.sh
@@ -5,7 +5,6 @@ set -e
 module_dir="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
 install() {
-  mas install 1423210932  # Flow
   defaults write design.yugen.Flow showWelcomeWindow -bool false  # Don't show welcome window
 }
 
