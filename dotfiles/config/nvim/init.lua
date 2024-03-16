3d5c468d79471fe759fd23b9d09ab1b7ad66703c
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-16 13:49:39 +0300

    Add `lazy.nvim`

---
+++ dotfiles/config/nvim/init.lua
@@ -0,0 +1,12 @@
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazy_path) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		"https://github.com/folke/lazy.nvim",
+		lazy_path,
+	})
+end
+vim.opt.rtp:prepend(lazy_path)
