88a9b9eba6e60e53f6345f67858caf2fb324ff8b
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-16 23:10:37 +0300

    Restructure `plugins`

--- dotfiles/config/nvim/lua/user/plugins/init.lua
+++ dotfiles/config/nvim/lua/user/plugins/init.lua
@@ -1,31 +1,5 @@
-require("user.plugins.init_utils").create_lazy_file_event()
+require("user.plugins.utils").create_lazy_file_event()
 
 require("lazy").setup({
-	{
-		url = "https://github.com/folke/tokyonight.nvim",
-		lazy = false,
-		priority = math.huge,
-		config = function()
-			vim.cmd.colorscheme("tokyonight-night")
-		end,
-	},
-	-- {
-	-- 	url = "https://github.com/nvim-treesitter/nvim-treesitter",
-	-- 	event = { "LazyFile" }, -- TODO: Consider `{ "BufReadPost", "BufNewFile" }` instead
-	-- 	build = function()
-	-- 		vim.cmd.TSUpdate()
-	-- 	end,
-	-- 	opts = function()
-	-- 		return require("user.plugins.configs.nvim-treesitter")
-	-- 	end,
-	-- 	config = function(_, opts)
-	-- 		require("nvim-treesitter.configs").setup(opts)
-	-- 	end,
-	-- },
-	{
-		url = "https://github.com/nvim-tree/nvim-web-devicons",
-	},
-	{
-		url = "https://github.com/nvim-lua/plenary.nvim",
-	},
+	{ import = "user.plugins.base" },
 })
