5142566f48c20324b656f2314d313a973725ec66
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-17 05:25:45 +0300

    Comment out broken configuration for now

--- dotfiles/config/nvim/lua/user/plugins/init.lua
+++ dotfiles/config/nvim/lua/user/plugins/init.lua
@@ -3,12 +3,12 @@ require("user.plugins.utils").create_lazy_file_event()
 require("lazy").setup({
 	spec = {
 		{ import = "user.plugins.any" },
-		{ import = "user.plugins.javascript" },
-		{ import = "user.plugins.lua" },
-		{ import = "user.plugins.python" },
-		{ import = "user.plugins.rust" },
-		{ import = "user.plugins.tailwind" },
-		{ import = "user.plugins.vue" },
+		-- { import = "user.plugins.javascript" },
+		-- { import = "user.plugins.lua" },
+		-- { import = "user.plugins.python" },
+		-- { import = "user.plugins.rust" },
+		-- { import = "user.plugins.tailwind" },
+		-- { import = "user.plugins.vue" },
 	},
 	defaults = {
 		lazy = true, -- TODO: Review
