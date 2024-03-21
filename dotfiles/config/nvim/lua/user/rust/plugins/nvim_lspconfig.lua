642da3f23d04e387a99fc0979265c2a1e55ea708
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-22 00:23:18 +0500

    Use 'rust_analyzer' for formatting in Neovim

--- dotfiles/config/nvim/lua/user/rust/plugins/nvim_lspconfig.lua
+++ dotfiles/config/nvim/lua/user/rust/plugins/nvim_lspconfig.lua
@@ -1,6 +1,16 @@
 local opts = {
 	x_servers = {
-		rust_analyzer = {},
+		rust_analyzer = {
+			on_attach = function(client, bufnr)
+				vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+					group = vim.api.nvim_create_augroup("user_rust_plugins_nvim_lspconfig", {}),
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ bufnr = bufnr, name = "rust_analyzer" })
+					end,
+				})
+			end
+		},
 	},
 }
 
