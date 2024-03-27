2cfcfe16bb0b323933651bbffdce394311f5a8b0
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-27 11:35:17 +0300

    Restore the file search key binding in Neovim

--- dotfiles/config/nvim/lua/user/base/plugins/fzf_lua.lua
+++ dotfiles/config/nvim/lua/user/base/plugins/fzf_lua.lua
@@ -24,16 +24,12 @@ local config_keymaps = function()
 		require("fzf-lua").buffers()
 	end, { silent = true })
 
-	vim.keymap.set({ "n" }, "<leader>sF", function()
-		require("fzf-lua").files()
-	end, { silent = true })
-
 	vim.keymap.set({ "n" }, "<leader>sL", function()
 		require("fzf-lua").live_grep_native()
 	end, { silent = true })
 
 	vim.keymap.set({ "n" }, "<leader>sf", function()
-		require("fzf-lua").git_files()
+		require("fzf-lua").files()
 	end, { silent = true })
 
 	vim.keymap.set({ "n" }, "<leader>sh", function()
