f3e38b842920089a67de99d7347c7b1eb399f4ef
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-04-13 00:47:54 +0300

    Configure Python in Neovim

---
+++ dotfiles/config/nvim/lua/user/python.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    "NvChad",
+    opts = {
+      inits_by_ft = {
+        python = {
+          function()
+            vim.opt_local.colorcolumn = "88"
+            vim.opt_local.expandtab = true
+            vim.opt_local.shiftwidth = 4
+            vim.opt_local.softtabstop = 4
+            vim.opt_local.tabstop = 4
+          end,
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "python",
+        "toml",
+      },
+    },
+  },
+  {
+    "mason.nvim",
+    opts = {
+      ensure_installed = {
+        "debugpy",
+        "pyright",
+        "ruff",
+        "ruff-lsp",
+      },
+    },
+  },
+  {
+    "nvim-lspconfig",
+    opts = {
+      ensure_configured = {
+        pyright = {},
+        ruff_lsp = {
+          on_attach = function(client, _)
+            require("nvchad.configs.lspconfig").on_attach(client, _)
+            if client.name == "ruff_lsp" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end,
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      require("dap-python").setup(path .. "/venv/bin/python")
+    end,
+  },
+  {
+    "conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff" },
+      },
+    },
+  },
+}
