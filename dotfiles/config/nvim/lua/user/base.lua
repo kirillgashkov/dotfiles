4cfbd0df4937cbe863cd8c0b61fddb3778099e34
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-09 00:00:20 +0300

    Add hover keymap to Neovim

--- dotfiles/config/nvim/lua/user/base.lua
+++ dotfiles/config/nvim/lua/user/base.lua
@@ -16,14 +16,20 @@ return {
       ensure_configured = {}, -- User-defined
     },
     config = function(_, opts)
-      require("nvchad.configs.lspconfig").defaults()
+      -- https://github.com/NvChad/NvChad/blob/020b8e4428d6d6ed3cf1d6afc899cb2f76aab1a0/lua/nvchad/configs/lspconfig.lua#L64
+      -- require("nvchad.configs.lspconfig").defaults()
+      dofile(vim.g.base46_cache .. "lsp")
+
       local on_attach = require("nvchad.configs.lspconfig").on_attach
       local on_init = require("nvchad.configs.lspconfig").on_init
       local capabilities = require("nvchad.configs.lspconfig").capabilities
 
       for server, server_opts in pairs(opts.ensure_configured) do
         require("lspconfig")[server].setup(vim.tbl_deep_extend("force", {
-          on_attach = on_attach,
+          on_attach = function(client, buffer)
+            on_attach(client, buffer)
+            vim.keymap.set({ "n" }, "K", vim.lsp.buf.hover, { buffer = buffer, silent = true })
+          end,
           on_init = on_init,
           capabilities = vim.deepcopy(capabilities),
         }, server_opts))
