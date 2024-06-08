014d344a8ca52b267b07524e8fa6150ae4ffc58d
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-08 23:59:29 +0300

    Add TOML and YAML to Neovim

--- dotfiles/config/nvim/lua/user/text.lua
+++ dotfiles/config/nvim/lua/user/text.lua
@@ -7,9 +7,27 @@ return {
           function()
             vim.opt_local.colorcolumn = "80"
             vim.opt_local.expandtab = true
-            vim.opt_local.shiftwidth = 4
-            vim.opt_local.softtabstop = 4
-            vim.opt_local.tabstop = 4
+            vim.opt_local.shiftwidth = 2
+            vim.opt_local.softtabstop = 2
+            vim.opt_local.tabstop = 2
+          end,
+        },
+        toml = {
+          function()
+            vim.opt_local.colorcolumn = "80"
+            vim.opt_local.expandtab = true
+            vim.opt_local.shiftwidth = 2
+            vim.opt_local.softtabstop = 2
+            vim.opt_local.tabstop = 2
+          end,
+        },
+        yaml = {
+          function()
+            vim.opt_local.colorcolumn = "80"
+            vim.opt_local.expandtab = true
+            vim.opt_local.shiftwidth = 2
+            vim.opt_local.softtabstop = 2
+            vim.opt_local.tabstop = 2
           end,
         },
       },
@@ -20,6 +38,8 @@ return {
     opts = {
       ensure_installed = {
         "json",
+        "toml",
+        "yaml",
       },
     },
   },
