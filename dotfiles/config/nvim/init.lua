d00ed97626b37d26a3c6e790679177d5d7a41815
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-12 23:43:28 +0300

    Make x_inits extendable

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -31,21 +31,32 @@ require("lazy").setup({
 			name = "nvim",
 			dir = vim.fn.stdpath("config"),
 			lazy = false,
+			opts_extend = { "x_inits" },
 			opts = {
 				x_inits = {}, -- User-defined
 			},
 			config = function(_, opts)
-				local non_ft_init = opts.x_inits["*"] or function() end
-				opts.x_inits["*"] = nil
+				local ft_to_inits = {}
+				for _, ft_and_init in ipairs(opts.x_inits) do
+					local ft, init = ft_and_init[1], ft_and_init[2]
+					ft_to_inits[ft] = ft_to_inits[ft] or {}
+					table.insert(ft_to_inits[ft], init)
+				end
 
-				non_ft_init()
+				local non_ft_inits = ft_to_inits["*"] or {}
+				ft_to_inits["*"] = nil
+				for _, non_ft_init in ipairs(non_ft_inits) do
+					non_ft_init()
+				end
 
-				for ft, ft_init in pairs(opts.x_inits) do
+				for ft, ft_inits in pairs(ft_to_inits) do
 					vim.api.nvim_create_autocmd({ "FileType" }, {
 						group = vim.api.nvim_create_augroup("internal_nvim_x_inits_" .. ft, {}),
 						pattern = { ft },
 						callback = function()
-							ft_init()
+							for _, ft_init in ipairs(ft_inits) do
+								ft_init()
+							end
 						end,
 					})
 				end
--- dotfiles/config/nvim/lua/internal/spec/base.lua
+++ dotfiles/config/nvim/lua/internal/spec/base.lua
@@ -5,42 +5,45 @@ return {
 		"nvim",
 		opts = {
 			x_inits = {
-				["*"] = function()
-					vim.g.loaded_node_provider = 0
-					vim.g.loaded_perl_provider = 0
-					vim.g.loaded_python3_provider = 0
-					vim.g.loaded_ruby_provider = 0
+				{
+					"*",
+					function()
+						vim.g.loaded_node_provider = 0
+						vim.g.loaded_perl_provider = 0
+						vim.g.loaded_python3_provider = 0
+						vim.g.loaded_ruby_provider = 0
 
-					vim.opt.breakindent = true
-					vim.opt.colorcolumn = { "80" }
-					vim.opt.cursorline = true
-					vim.opt.cursorlineopt = "number"
-					vim.opt.expandtab = true
-					vim.opt.ignorecase = true
-					vim.opt.inccommand = "split"
-					vim.opt.mouse = "a"
-					vim.opt.number = true
-					vim.opt.report = 0
-					vim.opt.scrolloff = 10
-					vim.opt.shiftwidth = 4
-					vim.opt.showmode = false
-					vim.opt.signcolumn = "yes"
-					vim.opt.shortmess:append({ I = true })
-					vim.opt.smartcase = true
-					vim.opt.softtabstop = 4
-					vim.opt.splitbelow = true
-					vim.opt.splitright = true
-					vim.opt.tabstop = 4
-					vim.opt.timeout = false
-					vim.opt.undofile = true
-					vim.opt.updatetime = 250
+						vim.opt.breakindent = true
+						vim.opt.colorcolumn = { "80" }
+						vim.opt.cursorline = true
+						vim.opt.cursorlineopt = "number"
+						vim.opt.expandtab = true
+						vim.opt.ignorecase = true
+						vim.opt.inccommand = "split"
+						vim.opt.mouse = "a"
+						vim.opt.number = true
+						vim.opt.report = 0
+						vim.opt.scrolloff = 10
+						vim.opt.shiftwidth = 4
+						vim.opt.showmode = false
+						vim.opt.signcolumn = "yes"
+						vim.opt.shortmess:append({ I = true })
+						vim.opt.smartcase = true
+						vim.opt.softtabstop = 4
+						vim.opt.splitbelow = true
+						vim.opt.splitright = true
+						vim.opt.tabstop = 4
+						vim.opt.timeout = false
+						vim.opt.undofile = true
+						vim.opt.updatetime = 250
 
-					vim.keymap.set({ "n" }, "<Esc>", function()
-						vim.cmd.nohlsearch()
-					end, { silent = true })
+						vim.keymap.set({ "n" }, "<Esc>", function()
+							vim.cmd.nohlsearch()
+						end, { silent = true })
 
-					vim.keymap.set({ "n", "v" }, ",", '"+', { silent = true })
-				end,
+						vim.keymap.set({ "n", "v" }, ",", '"+', { silent = true })
+					end,
+				},
 			},
 		},
 	},
