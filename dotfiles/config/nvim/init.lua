ec4a90453d0d6eec04c38c8552c45f81df6f013b
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-12 20:59:23 +0300

    Set global variables and options

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,5 +1,14 @@
 local util = require("internal.util")
 
+-- Set variables.
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Install lazy.
 
 local lazyPath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- dotfiles/config/nvim/lua/internal/spec/base.lua
+++ dotfiles/config/nvim/lua/internal/spec/base.lua
@@ -1,1 +1,44 @@
-return {}
+return {
+	{
+		"nvim",
+		opts = function(_, opts)
+			return vim.tbl_deep_extend("force", opts, {
+				inits_by_ft = {
+					["*"] = vim.list_extend(opts.inits_by_ft["*"] or {}, {
+						function()
+							vim.opt.breakindent = true
+							vim.opt.colorcolumn = { "80" }
+							vim.opt.cursorline = true
+							vim.opt.cursorlineopt = "number"
+							vim.opt.expandtab = true
+							vim.opt.ignorecase = true
+							vim.opt.inccommand = "split"
+							vim.opt.mouse = "a"
+							vim.opt.number = true
+							vim.opt.relativenumber = true
+							vim.opt.report = 0
+							vim.opt.scrolloff = 10
+							vim.opt.shiftwidth = 4
+							vim.opt.showmode = false
+							vim.opt.signcolumn = "yes"
+							vim.opt.shortmess:append({ I = true })
+							vim.opt.smartcase = true
+							vim.opt.softtabstop = 4
+							vim.opt.splitbelow = true
+							vim.opt.splitright = true
+							vim.opt.tabstop = 4
+							vim.opt.timeout = false
+							vim.opt.undofile = true
+							vim.opt.updatetime = 250
+
+							vim.keymap.set({ "n" }, "<Esc>", function()
+								vim.cmd.nohlsearch()
+							end, { silent = true })
+							vim.keymap.set({ "n", "v" }, ",", '"+', { silent = true })
+						end,
+					}),
+				},
+			})
+		end,
+	},
+}
