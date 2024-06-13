257cdb174f5da06886f62becf148bbbd0f64354f
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-13 13:46:37 +0300

    Add git support

---
+++ dotfiles/config/nvim/lua/internal/spec/git.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		name = "nvim",
+		dir = vim.fn.stdpath("config"),
+		opts = {
+			x_inits = {
+				{
+					"gitcommit",
+					function()
+						vim.opt_local.colorcolumn = { "50", "72" }
+					end,
+				},
+			},
+		},
+	},
+	{
+		"nvim-treesitter",
+		opts = {
+			x_parsers = {
+				"gitcommit",
+			},
+		},
+	},
+}
