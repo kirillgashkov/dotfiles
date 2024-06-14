2e61782d89ec38c957eedb810d7e856f5703f269
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-14 12:37:37 +0300

    Configure gitsigns

---
+++ dotfiles/config/nvim/lua/internal/modules/core/gitsigns.lua
@@ -0,0 +1,17 @@
+return {
+	url = "https://github.com/lewis6991/gitsigns.nvim",
+	event = "LazyFile",
+	opts = {
+		signs = {
+			add = { text = "│" },
+			change = { text = "│" },
+			delete = { text = "" },
+			topdelete = { text = "" },
+			changedelete = { text = "│" },
+			untracked = { text = "│" },
+		},
+	},
+	config = function(_, opts)
+		require("gitsigns").setup(opts)
+	end,
+}
