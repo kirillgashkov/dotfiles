f9869e83b235349a7c0f32bda2fe5c7e8b84b620
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-10-15 18:25:21 +0300

    Clean lspconfig setup in nvim

--- dotfiles/config/nvim/lua/internal/modules/core/lspconfig.lua
+++ dotfiles/config/nvim/lua/internal/modules/core/lspconfig.lua
@@ -27,37 +27,38 @@ return {
 				"force",
 				{},
 				vim.lsp.protocol.make_client_capabilities()
-				-- require("cmp_nvim_lsp").default_capabilities() -- TODO
+			-- require("cmp_nvim_lsp").default_capabilities() -- TODO
 			),
 		}
 
 		for server, server_opts in pairs(opts.x_servers) do
-			local server_opts = vim.tbl_deep_extend("force", default_server_opts, server_opts)
+			server_opts = vim.tbl_deep_extend("force", default_server_opts, server_opts)
 
 			if server == "efm" then
-				local language_to_tools = {}
-				for _, language_and_tool in ipairs(server_opts.x_tools) do
-					local language, tool = language_and_tool[1], language_and_tool[2]
-					language_to_tools[language] = language_to_tools[language] or {}
-					table.insert(language_to_tools[language], tool)
+				local tools_from_language = {}
+				for _, lt in ipairs(server_opts.x_tools) do
+					local language, tool = lt[1], lt[2]
+					tools_from_language[language] = tools_from_language[language] or {}
+					table.insert(tools_from_language[language], tool)
 				end
 				server_opts.x_tools = nil
 
 				server_opts = vim.tbl_deep_extend("force", server_opts, {
-					filetypes = vim.tbl_keys(language_to_tools),
+					filetypes = vim.tbl_keys(tools_from_language),
 					settings = {
-						languages = language_to_tools,
+						languages = tools_from_language,
 					},
 				})
 			end
 
-			if server_opts.x_on_attach then
+			if server_opts.x_on_attach ~= nil then
 				local x_on_attach = server_opts.x_on_attach
+				server_opts.x_on_attach = nil
 
 				server_opts = vim.tbl_deep_extend("force", server_opts, {
 					on_attach = function(client, bufnr)
 						local ft = vim.bo[bufnr].filetype
-						local handler = x_on_attach[ft] or x_on_attach["*"] or function() end
+						local handler = x_on_attach[ft] or x_on_attach["*"] or function(_, _) end
 						handler(client, bufnr)
 					end,
 				})
@@ -69,16 +70,28 @@ return {
 		vim.api.nvim_create_autocmd({ "LspAttach" }, {
 			group = vim.api.nvim_create_augroup("internal_lspconfig_lspattach", {}),
 			callback = function(event)
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+						group = vim.api.nvim_create_augroup("internal_lspconfig_bufwritepre", {}),
+						buffer = event.buf,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = event.buf })
+							vim.diagnostic.show() -- HACK: https://github.com/neovim/neovim/issues/25014
+						end,
+					})
+				end
+
 				vim.keymap.set({ "n" }, "<leader>e", function()
 					vim.diagnostic.open_float()
 				end, { buffer = event.buf, silent = true })
 
 				vim.keymap.set({ "n" }, "[d", function()
-					vim.diagnostic.goto_prev()
+					vim.diagnostic.jump({ count = -1 })
 				end, { buffer = event.buf, silent = true })
 
 				vim.keymap.set({ "n" }, "]d", function()
-					vim.diagnostic.goto_next()
+					vim.diagnostic.jump({ count = 1 })
 				end, { buffer = event.buf, silent = true })
 
 				vim.keymap.set({ "n" }, "<leader>q", function()
@@ -128,18 +141,6 @@ return {
 				vim.keymap.set({ "n" }, "gS", function()
 					require("fzf-lua").lsp_live_workspace_symbols()
 				end, { buffer = event.buf, silent = true })
-
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-						group = vim.api.nvim_create_augroup("internal_lspconfig_bufwritepre", {}),
-						buffer = event.buf,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = event.buf })
-							vim.diagnostic.show() -- HACK: https://github.com/neovim/neovim/issues/25014
-						end,
-					})
-				end
 			end,
 		})
 
