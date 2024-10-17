d14a226c3c338180abb0e94c434c21ff4e8bce4d
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-10-17 14:45:21 +0300

    Add lualine to nvim

---
+++ dotfiles/config/nvim/lua/internal/modules/core/lualine.lua
@@ -0,0 +1,213 @@
+local colors = {
+	bg       = '#1f2335',
+	fg       = '#bbc2cf',
+	yellow   = '#ECBE7B',
+	cyan     = '#008080',
+	darkblue = '#081633',
+	green    = '#98be65',
+	orange   = '#FF8800',
+	violet   = '#a9a1e1',
+	magenta  = '#c678dd',
+	blue     = '#51afef',
+	red      = '#ec5f67',
+}
+
+local conditions = {
+	buffer_not_empty = function()
+		return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+	end,
+	hide_in_width = function()
+		return vim.fn.winwidth(0) > 80
+	end,
+	check_git_workspace = function()
+		local filepath = vim.fn.expand("%:p:h")
+		local gitdir = vim.fn.finddir(".git", filepath .. ";")
+		return gitdir and #gitdir > 0 and #gitdir < #filepath
+	end,
+}
+
+return {
+	url = "https://github.com/nvim-lualine/lualine.nvim",
+	lazy = false,
+	dependencies = { "nvim-web-devicons" },
+	opts = {
+		options = {
+			component_separators = "",
+			section_separators = "",
+			theme = {
+				normal = { c = { fg = colors.fg, bg = colors.bg } },
+				inactive = { c = { fg = colors.fg, bg = colors.bg } },
+			},
+		},
+		sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+	},
+	config = function(_, opts)
+		local function insert_left(component)
+			table.insert(opts.sections.lualine_c, component)
+		end
+
+		local function insert_right(component)
+			table.insert(opts.sections.lualine_x, component)
+		end
+
+		insert_left({
+			function()
+				return "▊"
+			end,
+			color = { fg = colors.blue },
+			padding = { left = 0, right = 1 },
+		})
+
+		insert_left({
+			function()
+				return ""
+			end,
+			color = function()
+				local mode_color = {
+					n = colors.red,
+					i = colors.green,
+					v = colors.blue,
+					[""] = colors.blue,
+					V = colors.blue,
+					c = colors.magenta,
+					no = colors.red,
+					s = colors.orange,
+					S = colors.orange,
+					[""] = colors.orange,
+					ic = colors.yellow,
+					R = colors.violet,
+					Rv = colors.violet,
+					cv = colors.red,
+					ce = colors.red,
+					r = colors.cyan,
+					rm = colors.cyan,
+					["r?"] = colors.cyan,
+					["!"] = colors.red,
+					t = colors.red,
+				}
+				return { fg = mode_color[vim.fn.mode()] }
+			end,
+			padding = { right = 1 },
+		})
+
+		insert_left({
+			"filesize",
+			cond = conditions.buffer_not_empty,
+		})
+
+		insert_left({
+			"filename",
+			cond = conditions.buffer_not_empty,
+			color = { fg = colors.magenta, gui = "bold" },
+		})
+
+		insert_left({ "location" })
+
+		insert_left({ "progress", color = { fg = colors.fg, gui = "bold" } })
+
+		insert_left({
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			symbols = { error = " ", warn = " ", info = " " },
+			diagnostics_color = {
+				error = { fg = colors.red },
+				warn = { fg = colors.yellow },
+				info = { fg = colors.cyan },
+			},
+		})
+
+		insert_left({
+			function()
+				return "%="
+			end,
+		})
+
+		insert_left({
+			function()
+				local clients = vim.lsp.get_clients({ bufnr = 0 })
+
+				local lsps = ""
+				local efm_lsp = ""
+				for _, client in ipairs(clients) do
+					if #lsps ~= 0 then
+						lsps = lsps .. " "
+					end
+					if client.name == "efm" then
+						efm_lsp = client.name
+					else
+						lsps = lsps .. client.name
+					end
+				end
+				if #efm_lsp ~= 0 then
+					if #lsps ~= 0 then
+						lsps = lsps .. " "
+					end
+					lsps = lsps .. efm_lsp
+				end
+
+				if #lsps == 0 then
+					return "None"
+				end
+				return lsps
+			end,
+			icon = " LSP:",
+			color = { fg = "#ffffff", gui = "bold" },
+		})
+
+		insert_right({
+			"o:encoding",
+			fmt = string.upper,
+			cond = conditions.hide_in_width,
+			color = { fg = colors.green, gui = "bold" },
+		})
+
+		insert_right({
+			"fileformat",
+			fmt = string.upper,
+			icons_enabled = false,
+			color = { fg = colors.green, gui = "bold" },
+		})
+
+		insert_right({
+			"branch",
+			icon = "",
+			color = { fg = colors.violet, gui = "bold" },
+		})
+
+		insert_right({
+			"diff",
+			symbols = { added = " ", modified = "󰝤 ", removed = " " },
+			diff_color = {
+				added = { fg = colors.green },
+				modified = { fg = colors.orange },
+				removed = { fg = colors.red },
+			},
+			cond = conditions.hide_in_width,
+		})
+
+		insert_right({
+			function()
+				return "▊"
+			end,
+			color = { fg = colors.blue },
+			padding = { left = 1 },
+		})
+
+		require("lualine").setup(opts)
+	end,
+}
