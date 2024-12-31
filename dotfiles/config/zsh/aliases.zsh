4698c856fb5ec23415b7bf3c418499d3ddac3323
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-12-31 14:01:22 +0500

    Add git-clone-with-filter alias

--- dotfiles/config/zsh/aliases.zsh
+++ dotfiles/config/zsh/aliases.zsh
@@ -29,6 +29,7 @@ alias ll="ls -lhFA --group-directories-first"
 alias bat="bat --paging=never"
 alias go-build-with-debug='go build -gcflags=all="-N -l"' # see https://github.com/golang/vscode-go/wiki/debugging#attach
 alias go-run-with-debug='go run -gcflags=all="-N -l"'     # see https://github.com/golang/vscode-go/wiki/debugging#attach
+alias git-clone-with-filter='git clone --filter=blob:none'
 
 alias '??'="gh copilot suggest -t shell"
 alias iconv-windows-1251-to-utf-8="iconv -f WINDOWS-1251 -t UTF-8"
