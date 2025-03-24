b91227527ba061a385471c9496c1e0589988b62c
Kirill Gashkov <k.e.gashkov@gmail.com>
2025-03-24 21:57:50 +0300

    Disable nvim gitsigns

--- dotfiles/config/nvim/lua/internal/modules/core/gitsigns.lua
+++ dotfiles/config/nvim/lua/internal/modules/core/gitsigns.lua
@@ -1,17 +1,17 @@
 return {
-	url = "https://github.com/lewis6991/gitsigns.nvim",
-	event = { "LazyFile", "VeryLazy" },
-	opts = {
-		signs = {
-			add = { text = "│" },
-			change = { text = "│" },
-			delete = { text = "│" },
-			topdelete = { text = "│" },
-			changedelete = { text = "│" },
-			untracked = { text = "│" },
-		},
-	},
-	config = function(_, opts)
-		require("gitsigns").setup(opts)
-	end,
+	-- 	url = "https://github.com/lewis6991/gitsigns.nvim",
+	-- 	event = { "LazyFile", "VeryLazy" },
+	-- 	opts = {
+	-- 		signs = {
+	-- 			add = { text = "│" },
+	-- 			change = { text = "│" },
+	-- 			delete = { text = "│" },
+	-- 			topdelete = { text = "│" },
+	-- 			changedelete = { text = "│" },
+	-- 			untracked = { text = "│" },
+	-- 		},
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("gitsigns").setup(opts)
+	-- 	end,
 }
